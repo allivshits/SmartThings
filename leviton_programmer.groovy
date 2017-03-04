31aa1355b21c5554bbf0752138088b257ea3a738
allivshits <allivshits@gmail.com>
2017-03-04 11:43:32 -0800

    Update leviton_programmer.groovy

--- leviton_programmer.groovy
+++ leviton_programmer.groovy
@@ -1,9 +1,4 @@
 /**
- *  Enhanced GE/Jasco Dimmer Switch
- *	Author: Ben W. (@desertBlade)
- *
- * Based off of the Dimmer Switch under Templates in the IDE 
- * Copyright (C) Ben W.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
